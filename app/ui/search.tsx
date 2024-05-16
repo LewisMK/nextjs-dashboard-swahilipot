9e782ea513f361f433c1e2c4c367773f0b489e0c
LewisMK <elmurimi@gmail.com>
2024-05-16 12:49:57 +0300

    Chapter 12

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -3,6 +3,7 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { StringLiteralLike } from 'typescript';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string}) {
   const searchParams = useSearchParams();
@@ -10,11 +11,15 @@ export default function Search({ placeholder }: { placeholder: string}) {
   const { replace } = useRouter();
 
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term) => {
 
-    console.log(`Searching...  ${term}`);
+  
+
+    // console.log(`Searching...  ${term}`);
 
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+    
     if (term) {
       params.set('query', term);
     }
@@ -22,7 +27,7 @@ export default function Search({ placeholder }: { placeholder: string}) {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
